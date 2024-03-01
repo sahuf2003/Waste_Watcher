feaed60f34ce18297e87e864bdff498d3e818501
rsayyed591 <rehansayyed591@gmail.com>
2024-03-02 04:52:16 +0530

    create react

---
+++ waste_watcher/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './style.css'
+
+const Navbar = () => (
+    <header className='navbar'>
+        <div className='title item'>wasteWatcher</div>
+        <div className='item'>About Us</div>
+        <div className='item'>Contact</div>
+        <div className='item'>Help</div>     
+
+    </header>
+);
+
+export default Navbar;
